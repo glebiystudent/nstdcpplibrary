f383ea48667f64016079ecb142c9b15162e9892e
glebiystudent <159919966+glebiystudent@users.noreply.github.com>
2024-06-07 23:13:46 +0300

    template for map variadic construction in progress

--- nstd/main/map.hpp
+++ nstd/main/map.hpp
@@ -3,10 +3,20 @@
 #include <unordered_map>
 #include <map>
 #include <utility>
-#include <tuple>
-#include <iostream>
-
 
 namespace nstd {
-
+    template<typename K, typename V, typename... Ts>
+    [[nodiscard]] inline std::unordered_map<K, V> unordered_map(Ts&&... ts) noexcept(true) {
+        std::unordered_map<K, V> ret;
+        ret.reserve(sizeof...(Ts) / 2);
+        
+        K key;
+        auto f = [&](const std::size_t idx, auto val) {
+            std::cout << idx << ": " << val << "\n";
+        };
+        [&]<std::size_t... I>(std::index_sequence<I...>) {
+            (f(I, std::forward<Ts>(ts)), ...);
+        }(std::make_index_sequence<sizeof...(Ts)>{});
+        return ret;
+    }
 }
