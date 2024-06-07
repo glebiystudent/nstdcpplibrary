b14929062edab927f5f58eb735a481416fc16b4c
glebiystudent <159919966+glebiystudent@users.noreply.github.com>
2024-06-07 22:28:56 +0300

    officially merged advanced_log into an original log version
    now it supports any type of strings, numbers, booleans, std::vector, std::unordered_map, std::map. and all of it is in the variadic fashion style!

--- nstd/extra/log.hpp
+++ nstd/extra/log.hpp
@@ -8,81 +8,79 @@ namespace nstd {
     }
 }
 #else
-#include <concepts>
 #include <iostream>
 #include <vector>
-#include <span>
 #include <unordered_map>
 #include <map>
-#include <ranges>
+#include <concepts>
 
 namespace nstd {
-    template<typename T, typename T_>
-    inline void log(const std::unordered_map<T, T_>& map) noexcept(true) {
-        for(const auto& [k, v] : map)
-            std::cout << k << ": " << v << "\n";
-    }
-
-    template<typename T, typename T_>
-    inline void log(const std::map<T, T_>& map) noexcept(true) {
-        for(const auto& [k, v] : map)
-            std::cout << k << ": " << v << "\n";
-    }
-
-    template<typename T>
-    inline void log(const std::vector<T>& arr, const std::size_t end_newlines = 1, const std::string& spacer = " ") noexcept(true) {
-        for(const auto& e : arr)
-            std::cout << e << spacer;
-        for(std::size_t i = 0; i < end_newlines; ++i)
-            std::cout << "\n";
-    }
-
-    template<typename... Ts>
-        requires ((std::integral<Ts> || std::floating_point<Ts> || std::convertible_to<Ts, std::string>) && ...)
-    inline void log(Ts&&... vars) noexcept(true) {
-        ((std::cout << std::forward<Ts>(vars) << " "), ...);
-        std::cout << "\n";
-    }
-
+    // to_string wrapper
     template<typename T>
         requires std::integral<T> || std::floating_point<T>
-    std::string __str(T data) {
+    [[nodiscard]] inline std::string __str(const T& data) noexcept(true) {
         return std::to_string(data);
     }
     
-    std::string __str(const std::string& data) { 
+    [[nodiscard]] inline std::string __str(const std::string& data) noexcept(true) { 
         return data;
     }
 
-    // work in progress
+    // std::vector
     template<typename T>
         requires std::same_as<std::vector<typename T::value_type>, T>
-    [[nodiscard]] inline std::string __compose(T t) noexcept(true) {
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
         std::string ret = "";
-        for(const auto& e : t | std::views::take(t.size() - 1))
+        for(const auto& e : t)
             ret += __str(e) + ", ";
-        return "{" + ret + __str(t[t.size() - 1]) + "}";
+        return "[vector: {" + ret.substr(0, ret.size() - 2) + "}]";
     }
 
+    // unordered_map
+    template<typename T>
+        requires std::same_as<std::unordered_map<typename T::key_type, typename T::mapped_type>, T>
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
+        std::string ret = "";
+        for(const auto& [key, value] : t)
+            ret += "(" + __str(key) + ": " + __str(value) + "), ";
+    
+        return "[unordered_map: {" + ret.substr(0, ret.size() - 2) + "}]";
+    }
+    
+    // map
+    template<typename T>
+        requires std::same_as<std::map<typename T::key_type, typename T::mapped_type>, T>
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
+        std::string ret = "";
+        for(const auto& [key, value] : t)
+            ret += "(" + __str(key) + ": " + __str(value) + "), ";
+    
+        return "[map: {" + ret.substr(0, ret.size() - 2) + "}]";
+    }
+
+    // int / float / double / boolean
     template<typename T>
         requires std::integral<T> || std::floating_point<T>
-    [[nodiscard]] inline std::string __compose(T t) noexcept(true) {
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
         return std::to_string(t);
     }
 
+    // std::string
     template<typename T>
         requires std::assignable_from<std::string&, T>
-    [[nodiscard]] inline std::string __compose(T t) noexcept(true) {
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
         return t;
     }
 
+    // not recognized by the overloading system
     template<typename T>
-    [[nodiscard]] inline std::string __compose(T t) noexcept(true) {
+    [[nodiscard]] inline std::string __compose(const T& t) noexcept(true) {
         return "<undefined>";
     }
 
+    // the main log function
     template<typename... Ts>
-    inline void advanced_log(Ts&&... args) noexcept(true) {
+    inline void log(Ts&&... args) noexcept(true) {
         auto func = [&]<typename T>(T&& t){
             std::cout << __compose(std::forward<T>(t)) << " ";
         };
