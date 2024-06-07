4bdfb7d39622a0d4d7a1e4e1ff93c9ea948936a6
glebiystudent <159919966+glebiystudent@users.noreply.github.com>
2024-06-08 00:25:12 +0300

    reworked the unordered_map implementation + added <map>

--- nstd/main/map.hpp
+++ nstd/main/map.hpp
@@ -9,16 +9,42 @@
 #include <utility>
 
 namespace nstd {
-    // creates an unordered_map from your variadic arguments 
+    // creates an unordered_map from your variadic arguments: nstd::unordered_map<keytype, valuetype>(key, value, key, value, ...)
     template<typename K, typename V, typename... Ts>
     [[nodiscard]] inline std::unordered_map<K, V> unordered_map(Ts&&... ts) noexcept(true) {
-        std::unordered_map<K, V> ret;
-        ret.reserve(sizeof...(Ts) / 2);
-        
+        if(sizeof...(Ts) % 2 != 0)
+            return {};
+
+        std::unordered_map<K, V> ret; ret.reserve(sizeof...(Ts) / 2);
         K key;
         auto f = [&](const std::size_t idx, auto val) {
-            std::cout << idx << ": " << val << "\n";
+            if constexpr(std::is_same_v<decltype(val), K>)
+                key = val;
+            else
+                ret[key] = val;
         };
+
+        [&]<std::size_t... I>(std::index_sequence<I...>) {
+            (f(I, std::forward<Ts>(ts)), ...);
+        }(std::make_index_sequence<sizeof...(Ts)>{});
+        return ret;
+    }
+
+    // creates an unordered_map from your variadic arguments: nstd::unordered_map<keytype, valuetype>(key, value, key, value, ...)
+    template<typename K, typename V, typename... Ts>
+    [[nodiscard]] inline std::map<K, V> map(Ts&&... ts) noexcept(true) {
+        if(sizeof...(Ts) % 2 != 0)
+            return {};
+
+        std::map<K, V> ret; ret.reserve(sizeof...(Ts) / 2);
+        K key;
+        auto f = [&](const std::size_t idx, auto val) {
+            if constexpr(std::is_same_v<decltype(val), K>)
+                key = val;
+            else
+                ret[key] = val;
+        };
+
         [&]<std::size_t... I>(std::index_sequence<I...>) {
             (f(I, std::forward<Ts>(ts)), ...);
         }(std::make_index_sequence<sizeof...(Ts)>{});
