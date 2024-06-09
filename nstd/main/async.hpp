1d18dfbf22544d2217432ba4bf315d63b05a6891
glebiystudent <159919966+glebiystudent@users.noreply.github.com>
2024-06-09 18:50:54 +0300

    async module started

--- nstd/main/async.hpp
+++ nstd/main/async.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 // async.hpp
-// asynchronous addition to C++
+// easy asynchronous work added to C++
 
 
-#include <future>
+#include <thread>
+#include <utility>
 
 namespace nstd {
-
+    template<typename F, typename... Ts>
+    inline void async(F&& func, Ts&&... ts) noexcept(true) {
+        std::thread(func, std::forward<Ts>(ts)...).detach();
+    }
 }
