48c26b9d6866e98afe92d8a1dc6db0089c71c2c4
glebiystudent <159919966+glebiystudent@users.noreply.github.com>
2024-06-23 21:48:41 +0300

    filesystem start

--- nstd/main/filesystem.hpp
+++ nstd/main/filesystem.hpp
@@ -5,8 +5,54 @@
 
 
 #include <filesystem>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 namespace nstd {
+    inline void write(const std::string& path, const std::string& data) noexcept(true) {
+        std::ofstream f;
+        f.open(path);
+        if(!f.is_open()) return;
 
+        f << data;
+        f.close();
+    }
+
+    inline void write(const std::string& path, const std::vector<std::string>& data) noexcept(true) {
+        std::ofstream f;
+        f.open(path);
+        if(!f.is_open()) return;
+
+        for(const auto& e : data)
+            f << e << "\n";
+        f.close();   
+    }
+
+
+    [[nodiscard]] inline std::string read(const std::string& path) noexcept(true) {
+        std::ifstream f;
+        f.open(path, std::ios::in);
+        if(!f.is_open()) return "cannot open your file";
+
+        std::stringstream ret;
+        ret << f.rdbuf();
+        return ret.str();
+    }
+
+    [[nodiscard]] inline std::vector<std::string> read_lines(const std::string& path) noexcept(true) {
+        std::ifstream f;
+        f.open(path, std::ios::in);
+        if(!f.is_open()) return { "cannot open your file" };
+
+        std::vector<std::string> ret;
+        for(std::string line = ""; std::getline(f, line);)
+            ret.emplace_back(line + "\n");
+        return ret;
+    }
+
+    inline void create_directory(const std::string& path) noexcept(true) {
+        std::filesystem::create_directory(path);
+    }
 }
--- nstd/main/interval.hpp
+++ nstd/main/interval.hpp
@@ -7,6 +7,25 @@
 #include <thread>
 #include <chrono>
 
+#include <iostream>
+
+
 namespace nstd {
-    
+    template<typename F>
+    inline std::thread set_timeout(const int32_t ms, F&& f) noexcept(true) {
+        return std::thread([&]{
+            std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+            std::forward<F>(f)();
+        });
+    }
+
+    template<typename F>
+    inline std::thread set_interval(const int32_t ms, F&& f) noexcept(true) {
+        return std::thread([&]{
+            for(;;) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+                std::forward<F>(f)();
+            }
+        });
+    }
 }
